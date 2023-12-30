401775e2eca037aa4824602b0eac7d0bc218df48
Seongju38 <ljg013@naver.com>
2023-12-30 18:10:05 +0900

    게시물 등록 UI 제작

---
+++ my-app/pages/boards/new/index.js
@@ -0,0 +1,83 @@
+import {
+  Container,
+  Title,
+  WriterWrapper,
+  InputWrapper,
+  Label,
+  WriterInput,
+  TitleInput,
+  ContentInput,
+  ZipCodeWrapper,
+  ZipCodeInput,
+  ZipCodeButton,
+  AddressInput,
+  YoutubeInput,
+  ImageWrapper,
+  ImageUploadButton,
+  OptionWrapper,
+  RadioBoxWrapper,
+  RadioButton,
+  RadioLabel,
+  SubmitButton,
+} from "../../../styles/boards-new";
+
+export default function () {
+  return (
+    <Container>
+      <Title>게시물 등록</Title>
+      <WriterWrapper>
+        <InputWrapper>
+          <Label>작성자</Label>
+          <WriterInput type="text" placeholder="이름을 적어주세요." />
+        </InputWrapper>
+        <InputWrapper>
+          <Label>비밀번호</Label>
+          <WriterInput type="password" placeholder="비밀번호를 입력해주세요." />
+        </InputWrapper>
+      </WriterWrapper>
+      <InputWrapper>
+        <Label>제목</Label>
+        <TitleInput type="text" placeholder="제목을 작성해주세요." />
+      </InputWrapper>
+      <InputWrapper>
+        <Label>내용</Label>
+        <ContentInput type="text" placeholder="내용을 작성해주세요." />
+      </InputWrapper>
+      <InputWrapper>
+        <Label>주소</Label>
+        <ZipCodeWrapper>
+          <ZipCodeInput placeholder="07250" />
+          <ZipCodeButton>우편번호 검색</ZipCodeButton>
+        </ZipCodeWrapper>
+        <AddressInput />
+        <AddressInput />
+      </InputWrapper>
+      <InputWrapper>
+        <Label>유튜브</Label>
+        <YoutubeInput placeholder="링크를 복사해주세요." />
+      </InputWrapper>
+      <ImageWrapper>
+        <Label>사진 첨부</Label>
+        <ImageUploadButton>
+          +<br></br>Upload
+        </ImageUploadButton>
+        <ImageUploadButton>
+          +<br></br>Upload
+        </ImageUploadButton>
+        <ImageUploadButton>
+          +<br></br>Upload
+        </ImageUploadButton>
+      </ImageWrapper>
+      <OptionWrapper>
+        <Label>메인 설정</Label>
+        <RadioBoxWrapper>
+          <RadioButton type="radio" id="youtube" name="radio-box" />
+          <RadioLabel>유튜브</RadioLabel>
+          <RadioButton type="radio" id="photo" name="radio-box" />
+          <RadioLabel>사진</RadioLabel>
+        </RadioBoxWrapper>
+      </OptionWrapper>
+      <SubmitButton>등록하기</SubmitButton>
+    </Container>
+  );
+}
---
+++ my-app/styles/boards-new.js
@@ -0,0 +1,129 @@
+import styled from "@emotion/styled";
+
+export const Container = styled.div`
+  width: 1200px;
+  background: white;
+  box-shadow: 0px 4px 20px 0px rgba(0, 0, 0, 0.2);
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  /* align-items: center; */
+  padding: 60px 103px 100px 101px;
+  margin: 101px 160px 280px;
+`;
+
+export const Title = styled.div`
+  font-size: 36px;
+  font-weight: 700;
+  padding: 60px 0px 40px 412px;
+`;
+
+export const WriterWrapper = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+`;
+
+export const InputWrapper = styled.div`
+  padding-top: 40px;
+`;
+
+export const Label = styled.div`
+  font-size: 16px;
+  font-weight: 400px;
+  padding-top: 20px;
+  padding-bottom: 21px;
+`;
+
+export const WriterInput = styled.input`
+  width: 486px;
+  height: 52px;
+  border: 1px solid #bdbdbd;
+  margin-right: 12px;
+`;
+
+export const TitleInput = styled.input`
+  width: 996px;
+  height: 52px;
+  border: 1px solid #bdbdbd;
+`;
+
+export const ContentInput = styled.input`
+  width: 996px;
+  height: 480px;
+  border: 1px solid #bdbdbd;
+`;
+
+export const ZipCodeWrapper = styled.div`
+  display: flex;
+  flex-direction: row;
+`;
+
+export const ZipCodeInput = styled.input`
+  width: 77px;
+  height: 52px;
+  border: 1px solid #bdbdbd;
+  text-align: center;
+`;
+
+export const ZipCodeButton = styled.button`
+  padding: 14px 16px;
+  background: black;
+  color: white;
+  margin-left: 16px;
+`;
+
+export const AddressInput = styled.input`
+  width: 996px;
+  height: 52px;
+  border: 1px solid #bdbdbd;
+  margin-top: 16px;
+`;
+
+export const YoutubeInput = styled.input`
+  width: 996px;
+  height: 45.783px;
+  border: 1px solid #bdbdbd;
+`;
+
+export const ImageWrapper = styled.div`
+  padding-top: 40px;
+`;
+
+export const ImageUploadButton = styled.button`
+  width: 78px;
+  height: 78px;
+  background: #dbdbdb;
+  color: #4f4f4f;
+  font-size: 12px;
+  font-weight: 500;
+  margin-right: 24px;
+`;
+
+export const OptionWrapper = styled.div`
+  padding-top: 40px;
+`;
+
+export const RadioBoxWrapper = styled.div`
+  display: flex;
+`;
+
+export const RadioButton = styled.input`
+  width: 24px;
+  height: 24px;
+`;
+
+export const RadioLabel = styled.label`
+  font-size: 16px;
+  font-weight: 500;
+  margin-right: 20px;
+  margin-left: 8px;
+`;
+
+export const SubmitButton = styled.button`
+  padding: 14px 60px;
+  background: #ffd600;
+  margin: 80px 380px 100px 412px;
+  font-size: 16px;
+  font-weight: 500;
+`;
